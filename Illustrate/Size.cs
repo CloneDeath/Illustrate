d2693db99a42cdfb994f1937cf4deeee0ec80aec
Nicholas Rodine <halfofastaple@gmail.com>
2023-02-02 14:36:54 -0500

    Minor code cleanup

--- Illustrate/Size.cs
+++ Illustrate/Size.cs
@@ -1,17 +1,16 @@
-namespace Illustrate
+namespace Illustrate; 
+
+public struct Size
 {
-	public struct Size
-	{
-		public int Width { get; set; }
-		public int Height { get; set; }
+	public int Width { get; set; }
+	public int Height { get; set; }
 		
-		public Size(int width, int height) {
-			Width = width;
-			Height = height;
-		}
+	public Size(int width, int height) {
+		Width = width;
+		Height = height;
+	}
 		
-		public static Size operator +(Size left, Size right) {
-			return new Size(left.Width + right.Width, left.Height + right.Height);
-		}
+	public static Size operator +(Size left, Size right) {
+		return new Size(left.Width + right.Width, left.Height + right.Height);
 	}
 }
